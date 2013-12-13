7048e7f340b533e11c28b6b06af40e464a032252
Lam Hoang Ta <lam.hoang@gla.com.de>
2013-12-13 03:39:46 +0100

    test github

--- git.php
+++ git.php
@@ -1,4 +1,4 @@
 <?php 'git reset --hard HEAD'; ?>;
-<?php 'git pull'; ?>;
+<?php 'git pull origin develop'; ?>;
 <?php 'find . -type d -print0 | xargs -0 chmod 0755'; ?>
 <?php 'find . -type f -print0 | xargs -0 chmod 0644'; ?>
