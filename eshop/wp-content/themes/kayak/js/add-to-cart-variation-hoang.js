bee24f32fe23bcc3295cad47cb02e7fc01f01a15
Lam Hoang Ta <lam.hoang@gla.com.de>
2013-12-31 05:09:32 +0100

    fixed successfully responsive layout for color variant

--- eshop/wp-content/themes/kayak/js/add-to-cart-variation-hoang.js
+++ eshop/wp-content/themes/kayak/js/add-to-cart-variation-hoang.js
@@ -10,15 +10,14 @@
             $('a.avia-gallery-big img').attr('src',img_src);
         }
         
-        $('.avia-gallery-thumb a').css('width','30%');
-        $('#hoang_color_variant').css('top',$('.avia-gallery-big').height()+10);
+        $('.avia-gallery-thumb a').css('width','33%');
+        $('#hoang_color_variant').css('top',$('.avia-gallery-big').height()*1.015);
     	$('#hoang_color_variant').on("change",".hoang_variant_radio", function(e) {      /* whenever Customer click on color-img, do this function */
     		e.preventDefault();
     		$var_form = $('.variations select');
     		$var_form.val($(this).val());
     		$var_form.change();                               /* change value of "Color"-select form */
-    		$('.single_variation_wrap').css('display','inline');  /* fix bug for Quantity and Add to cart button */
-            $('p.stock').hide();                            /* Hide On Stock text */
+    		$('.single_variation_wrap').css('display','inline');  /* fix bug for Quantity and Add to cart button */                            /* Hide On Stock text */
             $('a.avia-gallery-big img').attr('src',$product_img.attr('src'));
     	});
     });
