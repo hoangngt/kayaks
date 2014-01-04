df614e6082345bd71f79178b5701df2aba25c9f4
Lam Hoang Ta <lam.hoang@gla.com.de>
2014-01-04 17:52:20 +0100

    modify by Color-variant: change price
    whenever Customer choose a variant

--- eshop/wp-content/themes/kayak/js/add-to-cart-variation-hoang.js
+++ eshop/wp-content/themes/kayak/js/add-to-cart-variation-hoang.js
@@ -8,12 +8,15 @@
         img_src = $product_img.attr('src');
         if (img_src!='') {
             $('a.avia-gallery-big img').attr('src',img_src);
+        var $default_price = $('.single_variation > span.price').html();
+        if ($default_price != "") $('p.price').html($default_price);
         }
     	$(".hoang_variant").on("click", function(e) {      /* whenever Customer click on color-img, do this function */
-            $(this).find('.hoang_variant_radio').prop("checked", true);
+            $(this).find('.hoang_variant_radio').prop("checked", true);      
     		$var_form = $('.variations select');
     		$var_form.val($(this).attr("wert"));
     		$var_form.change();                 /* change value of "Color"-select form */
+            $('p.price').html($('.single_variation > span.price').html());   
             $("#selected_color_text").html("Ausgewählte Farbe: " + $(this).attr("color_name"));
     		$('.single_variation_wrap').css('display','inline');  /* fix bug for Quantity and Add to cart button */                            /* Hide On Stock text */
             $('a.avia-gallery-big img').attr('src',$product_img.attr('src'));
